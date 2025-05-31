51dd85c6dc606df7db7055cd97d75b8523fe132f
Keshav Kumar <56951340+Keshav1802@users.noreply.github.com>
2025-05-31 22:26:37 +0530

    update

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:keshav_s_application2/presentation/home_screen/home_screen.dart';
